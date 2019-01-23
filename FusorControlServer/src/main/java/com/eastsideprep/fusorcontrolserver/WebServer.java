be9147e0327b50ab10d98946ffdc1c8feaae5aa3
ditto2642 <ditto2642@gmail.com>
2019-01-23 14:58:00 -0800

    added basic arduino communication framework

--- FusorControlServer/src/main/java/com/eastsideprep/fusorcontrolserver/WebServer.java
+++ FusorControlServer/src/main/java/com/eastsideprep/fusorcontrolserver/WebServer.java
@@ -13,8 +13,9 @@ import com.fazecast.jSerialComm.*;
  */
 public class WebServer {
     
+    SerialPort arduino;
+    
     public WebServer (){
-        
     }
     
     public void init() {
@@ -22,5 +23,34 @@ public class WebServer {
         staticFiles.location("/public");
         get("/", (req, res) -> "<h1><a href='index.html'>Go to index.html</a></h1>");
         get("/kill", (req, res) -> {stop(); System.out.println("Server ended with /kill"); return "server ended";});
+        get("/inita", (req, res) -> serialInit()?"success":"serial init failed");
     }
+    
+    public boolean serialInit() {
+        try {
+        arduino = SerialPort.getCommPorts()[0];
+        arduino.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_AVAILABLE; }
+            
+            @Override
+            public void serialEvent(SerialPortEvent e) {
+                if(e.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE) 
+                    return;
+                byte[] data = new byte[arduino.bytesAvailable()];
+                int count = arduino.readBytes(data, data.length);
+                System.out.println("Read " + count + " bytes from arduino");
+                handle(new String(data));
+            }
+        });
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+    
+    public void handle(String arg) {
+        
+    }
+    
 }
