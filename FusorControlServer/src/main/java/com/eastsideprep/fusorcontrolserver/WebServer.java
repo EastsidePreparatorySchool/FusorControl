384db44eccbc167f5add8bc0cf1d67363355e110
Faye Zhang <fzhang@eastsideprep.org>
2019-05-01 16:49:28 -0700

    finished JSON & getStatus

--- FusorControlServer/src/main/java/com/eastsideprep/fusorcontrolserver/WebServer.java
+++ FusorControlServer/src/main/java/com/eastsideprep/fusorcontrolserver/WebServer.java
@@ -7,7 +7,9 @@ package com.eastsideprep.fusorcontrolserver;
 
 import static spark.Spark.*;
 import com.fazecast.jSerialComm.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 /**
@@ -35,8 +37,7 @@ public class WebServer {
         get("/", (req, res) -> "<h1><a href='index.html'>Go to index.html</a></h1>");
         get("/kill", (req, res) -> {stop(); System.out.println("Server ended with /kill"); return "server ended";});
         get("/inita", (req, res) -> serialInit()?"success":"serial init failed");
-        
-        get("/getstatus", "application/json", (req, res) -> getStatus(req, res), new JSONRT());)
+        get("/getstatus", "application/json", (req, res) -> getStatus(req, res), new JSONRT());
         /*get("/getstatus", (req, res) -> {
             boolean arcon = arduino != null;
             long start = System.currentTimeMillis();
@@ -58,7 +59,6 @@ public class WebServer {
             System.out.println(ret);
             return ret;
         });*/
-        
         get("/variac", (req, res) -> {
             Double variacValue = Double.parseDouble(req.queryParams("value"));
             //TODO: paul to connect this to the arduino
@@ -66,13 +66,14 @@ public class WebServer {
         });
     }
 
-    public static Object getStatus(spark.Request req, spark.Response res) {
+    public Object getStatus(spark.Request req, spark.Response res) {
         boolean arcon = arduino != null;
         long start = System.currentTimeMillis();
-        String tmps="";
-        String tmpv="";
+        String tmpv = "";
+        String tmps = "";
         write("GETstatusEND");
         String message = "hold";
+        List<Status> status = new ArrayList<>();
         //I plan on having a colon for between the key and number,
         //this takes all messages coming in on the buffer until 'statusend' and sends the correct values back to the client
         do {
@@ -83,10 +84,8 @@ public class WebServer {
                 tmps = message.substring(coli+1);
             }                
         } while(!message.equals("statusend")&&System.currentTimeMillis()-start<=serialTimeout);
-        String ret = "{ tmps:" + tmps + ", tmpv:" + tmpv + ", arcon:" + (arcon?1:0);
-        System.out.println(ret);
-        return ret;
-        return myMessages;
+        status.add(new Status((arcon?1:0), tmpv, tmps));
+        return status;
     }
     
     private boolean serialInit() {
