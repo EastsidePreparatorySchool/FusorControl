85f7cfc8768491cd8d2c687f60ca6004fde2329d
lgibbs-eps <36085900+lgibbs-eps@users.noreply.github.com>
2019-05-21 16:50:50 -0700

    Update SensorCode.ino

--- SensorCode/SensorCode.ino
+++ SensorCode/SensorCode.ino
@@ -25,6 +25,7 @@ FD: Fire Detector
 #define VM2pin A0
 #define XDpin A0
 #define PSpin A0
+#define GCpin 2 // must be either 2 or 3 
 
 
 int vm1;
@@ -33,35 +34,42 @@ int xd;
 int ps;
 int msElapsed;
 int readFreq = 1; //(in 100s of ms)
-int sendFreq = 1; 
-volatile int ticks = 0; // number of pulses from the geiger
+int sendFreq = 1;
+int Gcount = 0;
+int gc = 0;
+long lastRead = millis();
+long dt = 0;
+
 void setup() {
   // communicate with the computer 
   Serial.begin(9600);
-
-  attachInterrupt(digitalPinToInterrupt(pin), ISR, RISING);
-}
-void ISR() {
-  ticks++;
+  attachInterrupt(digitalPinToInterrupt(pin),geigerInterrupt(), RISING) //TODO: mode
 }
 
-
-
+void geigerInterrupt() {
+   Gcount++;
+}
 
 void readAnalog() {
   vm1 = analogRead(VM1pin);
   vm2 = analogRead(VM2pin);
   xd = analogRead(XDpin);
   ps = analogRead(PSpin);
+  long m = millis();
+  gc = Gcount;
+  Gcount = 0;
+  dt = m - lastRead;
+  lastRead = m;
 }
 
-void sendMessage(int tic) {
+void sendMessage() {
   String message = "statusbegin";
   message += "VM1:" + String(vm1);
   message += "VM2:" + String(vm2);
   message += "XD:" + String(xd);
   message += "PS:" + String(ps);
-  message += "Ticks: " + String(tic); 
+  message += "GC:" + String(gc);
+  message += "DT:" + String(dt); // DT = delta time = time between last two reads
   message += "statusend";
   Serial.println(message);
 }
@@ -72,14 +80,8 @@ void loop() {
   if(msElapsed % readFreq == 0){
     readAnalog();
   }
-
-  int tempticks = ticks;
-  ticks = 0;
   if(msElapsed % sendFreq == 0){
-    sendMessage(tempticks);
+    sendMessage();
   }
   delay(100);
 }
-
-
-
