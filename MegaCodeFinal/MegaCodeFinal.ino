83c5fe8108eebf7124b727bee0982df83a1fd7c4
lgibbs-eps <36085900+lgibbs-eps@users.noreply.github.com>
2019-05-22 16:28:37 -0700

    Revert "Merge branch 'master' of https://github.com/EastsidePreparatorySchool/FusorControl"
    This reverts commit 8c7d67e84ea927abb7758b8fd1df7f5e4010f5ac, reversing
    changes made to e9976b1483e4742666a9052e90e9c0f98446d93b.

--- MegaCodeFinal/MegaCodeFinal.ino
+++ MegaCodeFinal/MegaCodeFinal.ino
@@ -1,4 +1,4 @@
-#include <Queue.h>
+#include "NewQueue.h"
 
 Queue<String> commands;
 String buffs = "";
@@ -29,7 +29,7 @@ void loop()
   while(eindex != -1)
   {
     commands.enqueue(buffs.substring(0,eindex));
-    buffs = buffs.substring(eindex + 2);
+    buffs = buffs.substring(eindex +2);
     eindex = buffs.indexOf("END");
   }
 
@@ -53,9 +53,9 @@ void handleBuffer(String command)
   {
     if(cont.startsWith("volt")) 
     {
-      int volts = toInt(cont.substring(4,7));
+      int volts = cont.substring(4,7).toInt();
       voltage(volts);
-      Serial.println("setvoltageEND");
+      Serial.println("setvoltage"+String(volts)+"END");
     }
   }
 
