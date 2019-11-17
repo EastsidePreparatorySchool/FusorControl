3b8d552e10a72ff4dfd56fea3d131f63ba71c35c
gunnarmein <gmein@eastsideprep.org>
2019-11-16 22:57:16 -0800

    Beginnings of local config file

---
+++ FusorControlServer/src/main/java/com/eastsideprep/fusorcontrolserver/Config.java
@@ -0,0 +1,62 @@
+package com.eastsideprep.fusorcontrolserver;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Config {
+    public boolean fakeCoreDevices = true;
+    public boolean noLog = false;
+    public int logFreq = 10;
+    public boolean includeCoreStatus = true;
+    public boolean verbose = false;
+    public boolean superVerbose = false;
+    public boolean noBlueTooth = false;
+    
+    private static Gson gson = new Gson();
+
+    public  static Config configFromFile(String path, String fileName) {
+        String s= null;
+        Config config = null;
+
+        try {
+            FileReader in = new FileReader(path + fileName);
+            char[] buffer = new char[65000];
+            in.read(buffer);
+            in.close();
+            s = new String(buffer).trim();
+        } catch (FileNotFoundException e) {
+            try {
+                byte[] buffer = new byte[65000];
+                InputStream is = Config.class.getResourceAsStream("/resources/" + fileName);
+                is.read(buffer);
+                s = new String(buffer).trim();
+            } catch (Exception e2) {
+            }
+        } catch (IOException e3) {
+        }
+
+        if (s == null) {
+            return new Config();
+        }
+        
+        try {
+            //System.out.println("readConfigFile: parsing JSON string: "+config.substring(0, 100) + "...");
+            config = gson.fromJson(s, Config.class);
+        } catch (JsonSyntaxException e) {
+            System.out.println("config:readConfigFile:File parse error");
+            System.out.println("config:readConfigFile:     " + e.getMessage() + e.toString());
+        } catch (Exception e4) {
+            System.out.println("config:readConfigFile:File parse error");
+            System.out.println("config:readConfigFile:     " + e4.getMessage() + e4.toString());
+        }
+        return config;
+    }
+    
+    public String toJson() {
+        return gson.toJson(this, Config.class);
+    }
+}
--- FusorControlServer/src/main/java/com/eastsideprep/fusorcontrolserver/DeviceManager.java
+++ FusorControlServer/src/main/java/com/eastsideprep/fusorcontrolserver/DeviceManager.java
@@ -10,8 +10,6 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class DeviceManager {
 
