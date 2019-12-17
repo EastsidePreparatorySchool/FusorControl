873bbaee0003b41323743d3d3a0759ae7cfeda26
gunnarmein <gmein@eastsideprep.org>
2019-12-16 20:40:47 -0800

    Draft 1 needs testing

--- Arduino/Variac/Variac.ino
+++ Arduino/Variac/Variac.ino
@@ -17,11 +17,14 @@
 #define REL 9  // relay to cut power to controller
 #define POT A1 // potentiometer feedback
 
-void setup(){
+void setup() {
   fusorInit("VARIAC");
   fusorAddVariable("input_volts", FUSOR_VARTYPE_INT);
   fusorAddVariable("potentiometer", FUSOR_VARTYPE_INT);
   fusorAddVariable("pot_adc", FUSOR_VARTYPE_INT);
+  fusorAddVariable("emergency_zero", FUSOR_VARTYPE_BOOL);
+
+  fusorSetBoolVariable("emergency_zero", false);
 
   // stepper control for varaic
   pinMode(PUL, OUTPUT);
@@ -51,7 +54,7 @@ int voltsToPot(int volts) {
   return map(volts, MINVOLTS, MAXVOLTS, 0, 1024);
 }
 
-void setVoltage(int volts) {
+void setVoltage(int volts, bool emergency) {
   int pot;
   int targetPot = voltsToPot(volts);
   if (volts > MAXVOLTS) return;
@@ -61,14 +64,16 @@ void setVoltage(int volts) {
   digitalWrite(ENA, LOW);
   digitalWrite(REL, HIGH);
 
+  pot = analogRead(POT);
+  dif = targetPot - pot;
+
   long start = millis();
-  do {
+  while ( abs(dif) > 10 ) {
     // make sure do not spend forever in here
     if (millis() - start > 3000) {
       break;
     }
-    pot = analogRead(POT);
-    dif = targetPot - pot;
+
     digitalWrite(DIR, (dif < 0) ? LOW : HIGH);
 
     digitalWrite(PUL, HIGH);
@@ -78,8 +83,24 @@ void setVoltage(int volts) {
     // no fusorDelay..() here, can't afford to make this too long
     delayMicroseconds(200);
     // now we can take a little breath, use fusorDelay..()
-    fusorDelayMicroseconds(5); // just to give it a chance to drain the USB queue and provide status
-  } while ( abs(dif) > 10 );
+
+    pot = analogRead(POT);
+    dif = targetPot - pot;
+
+    // update so the client can keep track
+    fusorSetIntVariable("pot_adc", pot);
+    fusorSetIntVariable("potentiometer", potToVolts(pot));
+
+    // try to delay it to something like 5V/s
+    // needs testing
+    // math is like this: Needs to be 25x slower than without delay
+    // time in loop without delay = 400us (see above)
+    // time in loop needs to be around 10ms
+
+    if (!emergency) {
+      fusorDelay(10);
+    }
+  }
 
   //fusorSendResponse("after loop");
   digitalWrite(ENA, HIGH);
@@ -87,9 +108,11 @@ void setVoltage(int volts) {
   FUSOR_LED_OFF();
 }
 
+
+
 void zeroVoltage() {
   //set the variac as low as we can
-  setVoltage(MINVOLTS);
+  setVoltage(MINVOLTS, true);
 
   //drive down some bonus steps, so we get to actual zero
   FUSOR_LED_ON();
@@ -112,26 +135,35 @@ void zeroVoltage() {
 void loop() {
   // must do this in loop, the rest is optional
   fusorLoop();
-  
+
   updateAll();
   delay(5);
 }
 
 void updateAll() {
   int pot;
-  int volts; 
-  
+  int volts;
+
   // put our current potentiometer reading into "potentiometer"
+
+  // emergency shutdown
+  if (fusorVariableUpdated("emergency_zero")) {
+    bool shutdownNow = fusorGetBoolVariable("emergency_zero");
+    if (shutdownNow) {
+      zeroVoltage();
+      while (true); // we don't do anything after that. 
+    }
+  }
 
   // if "input_volts" was updated, set variac to that voltage
   if (fusorVariableUpdated("input_volts")) {
     volts = fusorGetIntVariable("input_volts");
-    setVoltage(volts);
+    setVoltage(volts, false); // in non-emergency mode
   }
-  
+
   pot = analogRead(POT);
   volts = potToVolts(pot);
 
   fusorSetIntVariable("pot_adc", pot);
-  fusorSetIntVariable("potentiometer",volts);
+  fusorSetIntVariable("potentiometer", volts);
 }
