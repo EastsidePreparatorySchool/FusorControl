c740a690313e27d4d3f0befe404b8d6233df2483
gunnarmein <gmein@eastsideprep.org>
2021-05-24 13:25:24 -0700

    distributing fusor.h

--- Arduino/copy_headers.cmd
+++ Arduino/copy_headers.cmd
@@ -1,13 +1,13 @@
 copy fusor.h variac
 copy fusor.h rp
-copy fusor.h tmp
-copy fusor.h gas
+copy fusor.h hv-relay
 
 copy fusor.h hv-highside
 copy fusor.h hv-lowside
-copy fusor.h gc-serial
-copy fusor.h gc-ttl
-copy fusor.h pn-junction
+copy fusor.h sensorarray
+
+copy fusor.h tmp
+copy fusor.h gas
 copy fusor.h pirani
 
 copy fusor.h generic
