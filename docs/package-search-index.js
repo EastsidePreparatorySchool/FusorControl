4c99805d3a2d8b5bb3053020144becbf42e32534
Arjun VedBrat <45187468+CoconutMacaroon@users.noreply.github.com>
2021-10-25 09:52:00 -0700

    Revert all the '[AUTOMATED] Update docs' commits
    This reverts commit b32bad0f3e0a950115c0a4d613ea3097d190fc85.

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.eastsideprep.cameras"},{"l":"com.eastsideprep.fusorcontrolserver"},{"l":"com.eastsideprep.fusorweblog"},{"l":"com.eastsideprep.serialdevice"},{"l":"com.eastsideprep.weblog"}]
