1f727c98b155aa91d1429c8861d84a3b68ecd1f4
Arjun V <45187468+CoconutMacaroon@users.noreply.github.com>
2022-03-08 16:46:03 -0800

    Update SensorArray.ino

--- Arduino/SensorArray/SensorArray.ino
+++ Arduino/SensorArray/SensorArray.ino
@@ -51,75 +51,120 @@ void loop() {
   delay(5);
 }
 
-void updateAll() {
-  static char text[12];
-  static char *str = text;
-  static float decayingAvgCps = 0;
-  const float newFraction = 0.1;
- 
-  // read the latest message from the serial GC if there is one
-  // format: low byte, high byte
-  int current, before;
-  if (Serial3.available()) {
-    int bytes = Serial3.available();
-    // drain the whole queue
-    while(Serial3.available()) {
-      before = current;
-      current = Serial3.read();
-      fusorSetIntVariable("lastByte", current);
-      fusorForceUpdate();
-    }
-    // but only report if we read 2 bytes exactly
-    if (bytes == 2) {
-      fusorSetIntVariable("gc1", (current * 256) + before);
-    }
-  }
+void updateAll()
+{
+    static char text[12];
+    static char *str = text;
+    static float decayingAvgCps = 0;
+    const float newFraction = 0.1;
 
-  // read the latest message from the PIN diode sensor if there is one
-  if (Serial1.available()) {   
-    while(Serial1.available()) {
-      // format: <x02>M:1.24<CR><LF>
-      // could assert "<0x02>M:" but won't
-      char b = Serial1.read();
-      if (b == 0x02) {
-        // STX (start of transmission), reset buffer
-        str = text;
-        *str = 0;
-      } else {
-        if (b == 0x0D) {
-          // CR, end of message
-          *str = 0; // insert 0 instead of <CR>
-          fusorSetFloatVariableFromString("pin", &text[2]); // skipping "M:"
-        } else {
-          *str++ = b;
+    // read the latest message from the serial GC if there is one
+    // format: low byte, high byte
+    bool haveByte1 = false;
+    int byte1, byte2;
+    long time1;
+    if (Serial3.available())
+    {
+        // if we don't have the first byte [of a pair]
+        if(!haveByte1) {
+            byte1 = Serial3.read();
+            haveByte1 = true;
+            time1 = micros();
+        } else { // if we do have the first byte
+            byte2 = Serial3.read();
+            long time2 = micros();
+
+            // if the time between the two bytes was less than 0.1 seconds
+            // then we assume the two bytes belong together
+            if(time1 - time2 < 100000) {
+                haveByte1 = false;
+                info1 = byte1;
+                info2 = byte2;
+                /*
+                   Take the 1's and 0's in the first byte, shift them to the
+                   left eight units, and then add to byte2. Assuming byte1 =
+                   10101010 and byte2 = 11100011, here's what that does. Note
+                   that multiplying by 256 is the same as doing a left bit-shift
+                   eight units
+
+                   10101010 << 8 = 1010101000000000
+                   
+                   1010101000000000
+                   +       11100011
+                   ----------------
+                   1010101011100011
+                 */
+                fusorSetIntVariable("gc1", (byte1 * 256) + byte2);
+            } else {
+                /*
+                 * if the two bytes were so far apart that we don't think they belong together
+                 * discard the lone byte, as we can't use it, and assume that the second byte
+                 * that we read was really the start of the next pair
+                 *
+                 * So, we say to take the last byte of the 'broken' pair, and assume it is the
+                 * start of the next pair
+                 */
+                byte1 = byte2;
+                time1 = time2;
+                haveByte1 = true;
+            }
         }
-      }
     }
-  }
 
-  //
-  // get the edge-detected Geiger counts
-  //
-  long now;
-  static long last = 0; 
-  int d2now, d3now;
-  float interval;
-  
-  now = millis();
-  if (now > last+1000)  // update only once per second
-  {
-    last = now;
-    noInterrupts();
-    d2now = d2;
-    d3now = d3;
-    d2 = 0;
-    d3 = 0;
-    interrupts();
-    
-    fusorSetFloatVariable("gc2", d2now); 
-    fusorSetFloatVariable("gc3", d3now); 
-  }
+    // read the latest message from the PIN diode sensor if there is one
+    if (Serial1.available())
+    {
+        while (Serial1.available())
+        {
+            // format: <x02>M:1.24<CR><LF>
+            // could assert "<0x02>M:" but won't
+            char b = Serial1.read();
+            if (b == 0x02)
+            {
+                // STX (start of transmission), reset buffer
+                str = text;
+                *str = 0;
+            }
+            else
+            {
+                if (b == 0x0D)
+                {
+                    // CR, end of message
+                    *str = 0;                                         // insert 0 instead of <CR>
+                    fusorSetFloatVariableFromString("pin", &text[2]); // skipping "M:"
+                }
+                else
+                {
+                    *str++ = b;
+                }
+            }
+        }
+    }
+
+    //
+    // get the edge-detected Geiger counts
+    //
+    long now;
+    static long last = 0;
+    int d2now, d3now;
+    float interval;
+
+    now = millis();
+    if (now > last + 1000) // update only once per second
+    {
+        last = now;
+        noInterrupts();
+        d2now = d2;
+        d3now = d3;
+        d2 = 0;
+        d3 = 0;
+        interrupts();
+
+        fusorSetFloatVariable("gc2", d2now);
+        fusorSetFloatVariable("gc3", d3now);
+    }
 }
+
 
 void ISR2() 
 {
